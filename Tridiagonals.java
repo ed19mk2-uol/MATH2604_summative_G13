8cbe76d1aa5d066bd2fe37351577be1ab75a4ed2
Shaieq Khan <ed19mk2@leeds.ac.uk>
2026-04-07 17:08:30 +0100

    Created a function to sum two given tri-diagonal matrices of dimension nxn

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -12,9 +12,26 @@ class Tridiagonals
         return false;
     }
 
-    static double[][] sum(double[][] a, double[][] b) 
+    static double[][] sum(double[][] a, double[][] b)
     {
-        return null;
+        if (a == null || b == null)
+        {
+            return null;
+        }
+
+        int n = a.length;
+
+        double[][] result = new double[n][n];
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                result[i][j] = a[i][j] + b[i][j];
+            }
+        }
+
+        return result;
     }
 
     static double[][] productWithDiagonal(double[][] d, double[] t) 
