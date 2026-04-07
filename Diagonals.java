f901b557fbf211ed90c6795ef09914b3e4fb7f09
Shaieq Khan <ed19mk2@leeds.ac.uk>
2026-04-07 13:56:21 +0100

    Added method outline for Diagonals class

--- Diagonals.java
+++ Diagonals.java
@@ -2,5 +2,23 @@ import static java.lang.Math.*;
 
 class Diagonals
 {
+    static double[] exampleMatrix()
+    {
+        return null;
+    }
 
+    static double[] sum(double[] a, double[] b)
+    {
+        return null;
+    }
+
+    static double[] product(double[] a, double[] b)
+    {
+        return null;
+    }
+
+    static double[] inverse(double[] a)
+    {
+        return null;
+    }
 }
