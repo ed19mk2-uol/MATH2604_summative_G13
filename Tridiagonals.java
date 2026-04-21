81682ee0903af56071e00e6d935070d436a9dd45
knws0510-uol <knws0510@leeds.ac.uk>
2026-04-21 21:51:00 +0100

    Updates isValidTridiagonal Function
    Have not included javadoc comment and proper comment yet. To work on later.

--- Tridiagonals.java
+++ Tridiagonals.java
@@ -4,11 +4,20 @@ class Tridiagonals
 {
     public static double[][] exampleMatrix(int n) 
     {
+        
         return null;
     }
 
-    static boolean isValidTridiagonal(double[][] a) 
+    public static boolean isValidTridiagonal(double[][] a) 
     {
+        if (a.length !=3) return false; // check so row is 3
+        if (a == null) return false; // check if everything is null
+        for (double[] row : a) { // check if any of the row are null
+            if (row == null) return false;
+
+        int n = a[1].length; 
+        if (n<1) return false; //safety check bcs matrix size must be at least 1, cant be 0
+        
         return false;
     }
 
