10c43ed2545facb38e5dff83304ac3dd51480a1f
knws0510-uol <knws0510@leeds.ac.uk>
2026-04-21 21:30:14 +0100

    Updated Javadoc comment that were lost during merging issue

--- Diagonals.java
+++ Diagonals.java
@@ -1,6 +1,11 @@
 import static java.lang.Math.*;
 
 class Diagonals
+    /**
+    * Returns a representation of the diagonal matrix given 
+    * The matrix is represented as a 1D array where each element corresponds to a diagonal entry
+    * @return a double[] one-dimensional array representing the diagonal entries
+    */
     public static double[] exampleMatrix()
     {
         // the matrix ix 5x5 and has 10,8,5,-10,7 on its diagonal and zero elsewhere
